36b9ca8c2742fe7cb247fe0069ee59a10b975c41
henry145145 <henry@tourmalinelabs.com>
2020-02-26 08:40:05 -0800

    first commit for custom nvd

---
+++ esm/vendor/superset/AnnotationTypes.js
@@ -0,0 +1,74 @@
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+function extractTypes(metadata) {
+  return Object.keys(metadata).reduce((prev, key) => {
+    const result = prev;
+    result[key] = key;
+    return result;
+  }, {});
+}
+
+export const ANNOTATION_TYPES_METADATA = {
+  FORMULA: {
+    value: 'FORMULA',
+    label: 'Formula'
+  },
+  EVENT: {
+    value: 'EVENT',
+    label: 'Event',
+    supportNativeSource: true
+  },
+  INTERVAL: {
+    value: 'INTERVAL',
+    label: 'Interval',
+    supportNativeSource: true
+  },
+  TIME_SERIES: {
+    value: 'TIME_SERIES',
+    label: 'Time Series'
+  }
+};
+export const ANNOTATION_TYPES = extractTypes(ANNOTATION_TYPES_METADATA);
+export const DEFAULT_ANNOTATION_TYPE = ANNOTATION_TYPES.FORMULA;
+export const ANNOTATION_SOURCE_TYPES_METADATA = {
+  NATIVE: {
+    value: 'NATIVE',
+    label: 'Superset annotation'
+  }
+};
+export const ANNOTATION_SOURCE_TYPES = extractTypes(ANNOTATION_SOURCE_TYPES_METADATA);
+export function requiresQuery(annotationSourceType) {
+  return !!annotationSourceType;
+}
+const NATIVE_COLUMN_NAMES = {
+  descriptionColumns: ['long_descr'],
+  intervalEndColumn: 'end_dttm',
+  timeColumn: 'start_dttm',
+  titleColumn: 'short_descr'
+};
+export function applyNativeColumns(annotation) {
+  if (annotation.sourceType === ANNOTATION_SOURCE_TYPES.NATIVE) {
+    return _extends({}, annotation, {}, NATIVE_COLUMN_NAMES);
+  }
+
+  return annotation;
+}
+export default ANNOTATION_TYPES;
